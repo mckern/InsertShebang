321afc428d6a9dc560e17bc277794baecfa3e9b1
Ryan McKern <ryan@orangefort.com>
2018-10-01 01:59:17 -0700

    Accept an argument for shebang lines
    Instead of assuming `bash`, users can now specify the interpreter
    that a script should use.

--- insert_shebang.py
+++ insert_shebang.py
@@ -1,7 +1,12 @@
 import sublime
 import sublime_plugin
 
-
 class InsertShebangCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.insert(edit, 0, "#!/usr/bin/env bash\n")
+        window = self.view.window()
+        window.show_input_panel("Interpreter:", "bash",
+                                self.on_done, None, None)
+
+    def on_done(self, input_string):
+        self.view.run_command("move_to", {"to": "bof"})
+        self.view.run_command("insert", {"characters": "#!/usr/bin/env " + input_string + "\n"})
